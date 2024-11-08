b2c6b8d0734a7d66ddb6da8e1e39e319802ab65b
AbdulazeezwithWerr <abdulazizmamdaliyev16@gmail.com>
2024-11-08 18:18:57 +0500

    second

--- src/components/GetinTouch.jsx
+++ src/components/GetinTouch.jsx
@@ -1,27 +1,61 @@
-// import React from 'react'
-
-import { getText } from "../locales"
+import  { useState } from "react";
+import { getText } from "../locales";
 
 const GetinTouch = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <>
-        <div className="getInTouch">
-        <div className="container">
-         <div className="row align-items-center">
+      {isModalOpen && (
+        <div className="modal fade show" id="exampleModalCenter" tabIndex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="false" style={{ display: 'block' }}>
+          <div className="modal-dialog modal-dialog-centered" role="document">
+            <div className="modal-content">
+              <div className="modal-header">
+                <h5 className="modal-title" id="exampleModalLongTitle">Modal title</h5>
+                <button type="button" className="close" onClick={closeModal} aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div className="modal-body">
+                ...
+              </div>
+              <div className="modal-footer">
+                <button type="button" className="btn btn-secondary" onClick={closeModal}>
+                  Close
+                </button>
+                <button type="button" className="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
 
-         <div className="col-9">
-                <h3>{getText("getInTouch_Title")}</h3>
+      <div className="getInTouch">
+        <div className="container">
+          <div className="row align-items-center">
+            <div className="col-9">
+              <h3>{getText("getInTouch_Title")}</h3>
             </div>
             <div className="col-3">
-                <div className="button d-flex justify-content-end">
-                    <button>{getText("getInTouch_Button")}</button>
-                </div>
+              <div className="button d-flex justify-content-end">
+                <button type="button" className="btn btn-primary" onClick={openModal}>
+                  {getText("getInTouch_Button")}
+                </button>
+              </div>
             </div>
-         </div>
+          </div>
         </div>
-        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default GetinTouch
+export default GetinTouch;
